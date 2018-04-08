f2d26141f10208d0e74b0afeb890ba2f9b2291bc
DarthHamza <hamzamakia@gmail.com>
2018-04-08 17:33:48 +0300

    created a new app and created a test for it

---
+++ restaurants/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from django.urls import reverse
+
+class BusinessViewTestCase(TestCase):
+    def test_view_output(self):
+        url = reverse("hello-world")
+        response = self.client.get(url)
+        self.assertIn("Hello World!", response.context['msg'])
+        self.assertContains(response, "Hello World!")
+        self.assertEqual(response.status_code, 200)
