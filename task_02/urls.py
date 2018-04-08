70426802a1ef14f2c47154d98dd853f282d5749d
DarthHamza <hamzamakia@gmail.com>
2018-04-08 17:24:46 +0300

    renamed task_01 to task_02

--- task_02/urls.py
+++ task_02/urls.py
@@ -1,4 +1,4 @@
-"""task_01 URL Configuration
+"""task_02 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
