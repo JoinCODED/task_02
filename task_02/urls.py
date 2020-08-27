48673f07927d61b02b4cfe912c202bb0cc08b1e0
lailalelouch <39729644+lailalelouch@users.noreply.github.com>
2020-08-27 16:11:05 +0300

    Update urls.py

--- task_02/urls.py
+++ task_02/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from restaurants.views import x
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
